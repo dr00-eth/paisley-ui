1ae9c913da4a9d90e2cba059a1da077a01d5a9f6
Andrew Meyer <andrewmeyer23@gmail.com>
2023-03-23 15:58:55 -0500

    touch service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,12 @@
 self.addEventListener('install', (event) => {
-    // The install event is fired when the service worker is installed
-    // You can use this event to cache static assets
     //console.log('Service worker installed');
   });
   
   self.addEventListener('fetch', (event) => {
-    // The fetch event is fired whenever a network request is made
-    // You can use this event to handle caching strategies
     //console.log('Fetch event:', event);
   });
   
   self.addEventListener('activate', (event) => {
-    // The activate event is fired when the service worker is activated
-    // You can use this event to clean up old caches or perform other tasks
     //console.log('Service worker activated');
   });
   
